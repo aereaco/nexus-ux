e8735d26b70b0e0cb1b5ff26521e9b45e9683d33
aereaco <admin@aerea.co>
2026-08-12 13:08:10 -0500

    manifest.ts: code auto-snapshot
    - manifest.ts (code: +0/-0)

--- dist/nexus-ux.js
+++ dist/nexus-ux.js
@@ -2581,7 +2581,6 @@ ${scripts}
                         runtime.processElement(child);
                       }
                     });
-                    runtime.processElement(el);
                   }
                   const focusable = (config.shadowrootmode ? el.shadowRoot : el)?.querySelector("[autofocus], [data-autofocus]");
                   if (focusable instanceof HTMLElement) {
