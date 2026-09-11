be38c07cedb78e9b32a66929f518119e5476f444
aereaco <admin@aerea.co>
2026-09-10 23:19:12 -0500

    chore: remove deprecated site_old directory

--- site_old/demos/debug-drag.js
+++
@@ -1,7 +0,0 @@
-window.checkDragEffect = () => {
-    const dropZone = document.querySelector('[data-teleport\\:drop="list1"]');
-    const templateNode = dropZone.querySelector('[data-ux-template="true"]');
-    const EFFECT_RUNNERS_KEY = Symbol.for('__effect_runners__');
-    console.log("Template Node:", templateNode);
-    console.log("Effect runners:", templateNode ? templateNode[EFFECT_RUNNERS_KEY] : 'none');
-};
